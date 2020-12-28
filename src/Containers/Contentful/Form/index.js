3c5e9bbe32ecb13e683d7845de26ea3d12413634
Farid Hernandez <farid.hdz.64@gmail.com>
2020-12-28 12:36:16 -0600

    Day 10-3: Example using React-Query

--- src/Containers/Contentful/Form/index.js
+++ src/Containers/Contentful/Form/index.js
@@ -1,11 +1,30 @@
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
+import { StatusCritical, StatusGood } from 'grommet-icons';
 import React from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 import { useDispatch } from 'react-redux';
 import { createUser } from '../../../redux/thunks';
+import { createEntry } from '../sdk';
 import FormLayout from './FormLayout';
 
 function Form() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+
+  const queryClient = useQueryClient();
+
+  const postMutation = useMutation(
+    function (data) {
+      return createEntry(data);
+    },
+    {
+      onSuccess: function (args) {
+        queryClient.invalidateQueries('contentfulFetchUsers');
+      },
+      onError: function (err) {
+        console.error(err);
+      },
+    }
+  );
 
   const onSubmit = (values) => {
     const payload = {
@@ -15,11 +34,29 @@ function Form() {
 
     console.log('values submitted', values, payload);
 
-    dispatch(createUser(payload));
+    postMutation.mutate(payload);
+
+    // dispatch(createUser(payload));
   };
 
   return (
     <Box pad="medium" elevation="medium" gap="small" width="large">
+      {postMutation.isSuccess && (
+        <Box direction="row" gap="medium">
+          <Text color="brand">
+            User created successfully... <StatusGood color="brand" />
+          </Text>
+        </Box>
+      )}
+
+      {postMutation.isError && (
+        <Box direction="row" gap="medium">
+          <Text color="accent-1">
+            An error has occurred... <StatusCritical color="accent-1" />
+          </Text>
+        </Box>
+      )}
+
       <FormLayout onSubmit={onSubmit} />
     </Box>
   );
--- src/Containers/Contentful/List/index.js
+++ src/Containers/Contentful/List/index.js
@@ -1,22 +1,62 @@
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
+import { Refresh, StatusCritical } from 'grommet-icons';
 import React, { useEffect } from 'react';
+import { useQuery } from 'react-query';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../../../redux/thunks';
+import { getEntries } from '../sdk';
 
 import Table from './Table';
 
 function List() {
-  const searchString = useSelector((state) => state.data.searchString);
-  const users = useSelector((state) => state.data.users);
-  const dispatch = useDispatch();
+  // const searchString = useSelector((state) => state.data.searchString);
+  // const users = useSelector((state) => state.data.users);
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    // effect
-    dispatch(getUsers());
-  }, [dispatch, searchString]);
+  // useEffect(() => {
+  //   // effect
+  //   dispatch(getUsers());
+  // }, [dispatch, searchString]);
+
+  const { data: users, isFetching, isLoading, error, status } = useQuery(
+    'contentfulFetchUsers',
+    function () {
+      return getEntries({
+        content_type: 'users',
+      });
+    }
+  );
+
+  if (isLoading) {
+    return (
+      <Box direction="row" gap="medium">
+        <Text color="accent-1">
+          Loading... <Refresh color="accent-1" />
+        </Text>
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box direction="row" gap="medium">
+        <Text color="accent-2">
+          An error has occurred... <StatusCritical color="accent-2" />
+        </Text>
+      </Box>
+    );
+  }
 
   return (
     <Box pad="medium" elevation="medium" fill gap="small" width="large">
+      {isFetching && (
+        <Box direction="row" gap="medium">
+          <Text color="accent-2">
+            Fetching data... <Refresh color="accent-2" />
+          </Text>
+        </Box>
+      )}
+
       <Table users={users} />
     </Box>
   );
